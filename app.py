b2379d7fee318f0a244d54447d72e22c409ba9d8
Bob Ford <macbookair@Bobs-MacBook-Pro.local>
2025-06-18 21:49:14 -0400

    refactor: Defer payment method selection to Stripe Dashboard

--- app.py
+++ app.py
@@ -655,7 +655,6 @@ def create_checkout_session(user_id):
 
     try:
         checkout_session = stripe.checkout.Session.create(
-            payment_method_types=['card'],
 
             line_items=[
                 {
